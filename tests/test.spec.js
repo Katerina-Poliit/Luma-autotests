412913f25c86b91bf232325284d4c4596754b93d
Katerina Poliit <alexispoliyit@gmail.com>
2024-06-10 11:10:02 +0300

    hh

---
+++ tests/test.spec.js
@@ -0,0 +1,21 @@
+import { test, expect, request } from '@playwright/test'
+
+const baseApiURL = 'https://reqres.in/api'
+
+test('create user', async ({ request }) => {
+    const response = await request.post(`${baseApiURL}/register`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            "email": "eve.holt@reqres.in",
+            "password": "pistol"
+        }
+
+    })
+
+    //    console.log(response.ok());
+    expect(response.ok()).toBeTruthy();
+    expect(response.status()).toEqual(200);
+    // console.log(await response.json());
+})
