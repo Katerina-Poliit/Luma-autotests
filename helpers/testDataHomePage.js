324fdbdaa043392d7de247649bdfab61c56457f3
Alexander Saponov <98758052+AlexUA2022@users.noreply.github.com>
2024-06-14 07:43:11 +0300

    RF_POM | homePage.spec | ТС 03.1.3 Verify that the "What\'s New" navigation menu link is displayed and contains the appropriate text, ТС 03.1.4 Verify that the "Women" navigation menu link is displayed and contains the appropriate text, ТС 03.1.5 Verify that the "Men" navigation menu link is displayed and contains the appropriate text, ТС 03.1.6 Verify that the "Gear" navigation menu link is displayed and contains the appropriate text, ТС 03.1.7 Verify that the "Training" navigation menu link is displayed and contains the appropriate text, ТС 03.1.8 Verify that the "Sale" navigation menu link is displayed and contains the appropriate text (#74)

--- helpers/testDataHomePage.js
+++ helpers/testDataHomePage.js
@@ -4,4 +4,9 @@ export const BASE_URL = 'https://magento.softwaretestingboard.com';
 
 //test data
 
-// export const LOGO_ALIGNMENT = 'left';
+export const WHATS_NEW_LINK_TEXT = "What's New";
+export const WOMEN_LINK_TEXT = 'Women';
+export const MEN_NEW_LINK_TEXT = 'Men';
+export const GEAR_NEW_LINK_TEXT = 'Gear';
+export const TRAINING_NEW_LINK_TEXT = 'Training';
+export const SALE_NEW_LINK_TEXT = 'Sale';
