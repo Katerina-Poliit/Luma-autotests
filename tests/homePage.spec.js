1fa666b275c079209dd4f43cb60616e485ef1709
AlexUA2022 <avsaponov@gmail.com>
2024-06-14 05:59:40 +0300

    RF_POM | homePage.spec | ТС 03.1.1 Verify that user directed to the Home page after entering "Home page" url

---
+++ tests/homePage.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+import HomePage from "../page_objects/homePage";
+import { BASE_URL } from "../helpers/testDataHomePage";
+
+test.describe('homePage.spec', () => {
+	test.beforeEach(async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await homePage.open();
+
+	});
+
+	test('ТС 03.1.1 Verify that user directed to the Home page after entering "Home page" url', async ({ page }) => {
+		const homePage = new HomePage(page);
+
+		await expect(page).toHaveURL(BASE_URL);
+
+	});
+
+})
