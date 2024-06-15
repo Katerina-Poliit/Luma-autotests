f3e7908fdba8f09e199bf67bf8f376d90c0f80ae
Katerina Poliit <alexispoliyit@gmail.com>
2024-06-15 20:18:35 +0300

    RF_POM | homePage.spec |ТС 03.1.148 Verify that the "Tops" dropdown contains the "Tees" subcategory

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -93,7 +93,8 @@ class HomePage {
 		getMenDropdown: () => this.page.getByRole('menuitem', { name: ' Men' }),
 		getMenTopsSubcategory: () => this.page.locator('#ui-id-17'),
 		getMenTopsJackets:() => this.page.locator('#ui-id-19'),
-		getMenTopsHoodiesSweatshirts:() => this.page.locator('#ui-id-20')
+		getMenTopsHoodiesSweatshirts:() => this.page.locator('#ui-id-20'),
+		getMenTopsTees:() => this.page.locator('#ui-id-21'),
 
 
 	};
